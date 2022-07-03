95e7583b58778745d350c281096f01229f5f6117
Ganesan <connect2ganesan@gmail.com>
2022-07-03 23:03:23 +0530

    Checkout logic added

--- src/app/attendance-login/attendance-login.page.ts
+++ src/app/attendance-login/attendance-login.page.ts
@@ -3,6 +3,8 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { OfficeInService } from '../office-in.service';
 import { Router } from '@angular/router';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-attendance-login',
   templateUrl: './attendance-login.page.html',
@@ -14,6 +16,16 @@ export class AttendanceLoginPage implements OnInit  {
   longitude: any = 0; //longitude
   address;
   vilage;
+  currentTime;
+  currentDate;
+  checkIn: any = "00:00";
+  checkOut: any = "00:00";
+  remainingTime: any = "00:00";
+  options = {
+    timeout: 20000, 
+    enableHighAccuracy: true, 
+    maximumAge: 3600
+  };
   
   constructor(
     private geolocation: Geolocation,
@@ -22,20 +34,60 @@ export class AttendanceLoginPage implements OnInit  {
   ) {
     
   }
+
   ngOnInit() {
+  this.checkIn = "00:00";
+  this.checkOut = "00:00";
    this.getCurrentCoordinates();
+   setInterval(() => {
+    this.currentTime = moment(new Date).format("hh:mm:ss a");
+    this.currentDate = moment(new Date).format("DD MMM YYYY");
+    if(moment(new Date).isBefore(moment({ hour:23, minute: 59 }))) {
+     // console.log("yes")
+    
+      if(this.checkIn !== "00:00") {
+        var startTime = this.checkIn;
+        var endTime = moment(new Date).format("hh:mm");
+        var hours = moment
+                .duration(moment(endTime, 'hh:mm')
+                .diff(moment(startTime, 'hh:mm'))
+                );
+        //let hr = 
+        this.remainingTime = `${hours.hours()} : ${hours.minutes()}`;
+      }
+      //console.log(hours);
+    
+    }else {
+     // console.log("No")
+    }
+   },1000)
   }
-  options = {
-    timeout: 20000, 
-    enableHighAccuracy: true, 
-    maximumAge: 3600
-  };
 
   reloadCurrentRoute() {
     let currentUrl = this.router.url;
     this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
         this.router.navigate([currentUrl]);
     });
+  }
+
+  checkInCall() {
+    this.checkIn = moment(new Date).format("hh:mm");
+  }
+
+  checkOutCall() {
+    //this.router.navigate(['/', 'login']);
+    this.checkOut = moment(new Date).format("hh:mm");
+    var startTime = this.checkIn;
+    var endTime = this.checkOut;
+    var hours = moment
+            .duration(moment(endTime, 'hh:mm')
+            .diff(moment(startTime, 'hh:mm'))
+            );
+    this.remainingTime = `${hours.hours()} : ${hours.minutes()}`;
+  }
+
+  logOut() {
+    this.router.navigate(['/', 'login']);
   }
   
   // use geolocation to get user's device coordinates
